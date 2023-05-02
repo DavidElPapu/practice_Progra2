d5060a54f29ca1648a8d5a7b9b94f19160842279
josebarr03 <jose.barrios@uniat.edu.mx>
2023-05-02 09:02:07 -0600

    Clase de vectores, reservas e iteradores

--- practice_Progra2.cpp
+++ practice_Progra2.cpp
@@ -1,19 +1,197 @@
-// practice_Progra2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int askNumber(string question, int high, int low = 1);
+//GUESS MY NUMBER
+void guessMyNumber();
+void vectorsPart1();
+void vectorPractice();
+void vectorReserve();
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int NUM_SCORES = 4;
+    int score;
+
+    vector<int>::const_iterator iter;
+    vector<int> scores;
+
+    for (int i = 0; i < NUM_SCORES; i++)
+    {
+        cout << "Score " << i + 1 << endl;
+        cin >> score;
+        scores.push_back(score);
+    }
+
+    for (iter = scores.begin(); iter != scores.end(); iter++)
+    {
+        cout << *iter << endl;
+    }
+
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+void vectorReserve()
+{
+    vector<int> scores(10, 0);
+    cout << "Vector size is: " << scores.size() << endl;
+    cout << "Vector capacity is: " << scores.capacity() << endl;
+    scores.reserve(20);
+    scores.push_back(0);
+    cout << "Vector size is: " << scores.size() << endl;
+    cout << "Vector capacity is: " << scores.capacity() << endl;
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+    scores.push_back(3);
+    cout << "Vector size is: " << scores.size() << endl;
+    cout << "Vector capacity is: " << scores.capacity() << endl;
+
+}
+
+void vectorsPart1()
+{
+    //vector<string> myStuff = { "espada", "martillo", "bomba" };
+    //vector<string> inventory(10);
+    //vector<string> inventory(10, " Vacio ");
+    //vector<string> inventory(myStuff);
+
+
+    vector<string> inventory;
+
+    inventory.push_back("Espada");
+    inventory.push_back("Escudo");
+    inventory.push_back("Martillo");
+
+    cout << "Tienes " << inventory.size() << " items en tu inventario" << endl;
+
+    //Iteration FOR
+    for (unsigned int i = 0; i < inventory.size(); i++)
+    {
+        cout << inventory[i] << endl;
+    }
+
+    cout << "\nCambiaste tu " << inventory[0] << " por una Bomba.";
+    inventory[0] = "Bomba";
+
+    cout << "\n";
+
+    //Iteration DO-WHILE
+    int i = 0;
+    do {
+        cout << inventory[i] << endl;
+        i++;
+    } while (i < inventory.size());
+
+    cout << inventory[0] << " tiene " << inventory[0].size() << " letras en él ";
+
+    cout << " Perdiste un item en la última batalla " << endl;
+
+    //Delete item from vector
+    inventory.pop_back();
+    //Interation FOR
+    for (unsigned int i = 0; i < inventory.size(); i++)
+    {
+        cout << inventory[i] << endl;
+    }
+
+    cout << "Un enemigo ha robado todas tus armas" << endl;
+    //inventory.clear();
+}
+
+void vectorPractice()
+{
+    int invSize = 0;
+    string object = " ";
+    while (invSize <= 0 || invSize > 10)
+    {
+        system("cls");
+        cout << "Buenas, escriba el tamaño que quiere que tenga su inventario" << endl;
+        cin >> invSize;
+        if (invSize > 10)
+        {
+            cout << "Oye, eso es demasiado espacio, intenta poner un numero menor a 10 " << endl;
+        }
+        else if (invSize <= 0)
+        {
+            cout << "Enserio?, no puedes tener solo 0 espacios, vamos viejo" << endl;
+        }
+    }
+    vector<string> inventory(invSize);
+    system("cls");
+    cout << "Ahora toca llenar tu inventario de chucherias" << endl;
+    for (int i = 0; i < inventory.size(); i++)
+    {
+        cout << "Escribe el objeto que quieres meter" << endl;
+        cout << "(Te quedan " << inventory.size() - i << " espacios disponibles)" << endl;
+        cin >> inventory[i];
+        //inventory.push_back(object);
+    }
+    system("cls");
+    cout << "Este es tu inventario actualmente:" << endl;
+    for (int i = 0; i < inventory.size(); i++)
+    {
+        cout << i + 1 << ") " << inventory[i] << endl;
+    }
+    
+}
+
+
+
+int askNumber(string question, int high, int low)
+{
+    int number = 0;
+
+    do {
+        cout << question << "entre " << low << " y " << high << endl;
+        cin >> number;
+    } while (number > high || number < low);
+
+    return number;
+}
+
+
+void guessMyNumber()
+{
+    srand(static_cast<unsigned int>(time(0)));
+    //srand(time(NULL));
+    int randomNumber = rand();
+    int secretNumber = (randomNumber % 100) + 1;
+    int guess;
+    int tries = 0;
+    int veryClose;
+
+
+    cout << "GUESS MY NUMBER" << endl;
+    cout << "Adivina el número en el menor número de intentos posible." << endl;
+
+    //Depurar
+    cout << secretNumber;
+    do
+    {
+
+        guess = askNumber("\nIngresa un número ", 300);
+
+        tries++;
+
+        veryClose = secretNumber - guess;
+
+
+        if (veryClose <= 5 && veryClose >= -5 && veryClose != 0)
+        {
+            cout << "Muy cerca!!\n";
+        }
+
+        if (guess > secretNumber) {
+            cout << "Muy Alto\n\n";
+        }
+        else if (guess < secretNumber) {
+            cout << "Muy Bajo\n\n";
+        }
+        else {
+            cout << "Excelente lo hiciste en -- " << tries << "-- intentos";
+
+        }
+    } while (guess != secretNumber);
+}
